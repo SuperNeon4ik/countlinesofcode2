ff21ca9627ad2ab750dd345554318aee6b9a3929
Tautvydas <tautvydas.razmantas@gmail.com>
2026-05-06 21:08:16 +0300

    Update setup.bat (#1)
    Add pause

--- setup.bat
+++ setup.bat
@@ -6,3 +6,4 @@ echo
 echo Done! Now the program is ready to be run!
 echo Use 'run.bat' or 'run.sh' to run the application!
 echo Alternatively, use 'python index.py' (Windows) or sometimes 'python3 index.py' (Linux / MacOS) 
+pause
